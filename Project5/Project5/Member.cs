c96a45c90a47df5800728a3ea28c10b28e6b871e
ibra <dev@test.com>
2021-07-01 16:20:02 +0300

    Adding Member Class

---
+++ Project5/Project5/Member.cs
@@ -0,0 +1,14 @@
+﻿namespace Project5
+{
+    public class Member
+    {
+        private string name;
+        private string major;
+
+        public Member(string name, string major)
+        {
+            this.name = name;
+            this.major = major;
+        }
+    }
+}
