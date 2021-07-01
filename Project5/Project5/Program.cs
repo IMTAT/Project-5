3f5c4960939622ef45fd599011eeaad16a2bc36f
Taghreed <Taghreed.Alqahtani@tuwaiq.edu.sa>
2021-07-01 17:30:21 +0300

    createXmlFile method

--- Project5/Project5/Program.cs
+++ Project5/Project5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml;
 
 namespace Project5
 {
@@ -10,13 +11,33 @@ namespace Project5
             List<Member> members = new List<Member>()
             {
                 new Member("IBRA", "Computer Eng"),
-                new Member("IBRA", "Computer Eng"),
+                new Member("TAGHREED", "Computer Science"),
                 new Member("IBRA", "Computer Eng"),
                 new Member("IBRA", "Computer Eng"),
                 new Member("IBRA", "Computer Eng"),
             };
+            CreateXMLFile();
         }
-        
-        
+
+        public static void CreateXMLFile()
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            XmlElement root = xmldoc.CreateElement("email");
+            xmldoc.AppendChild(root);
+
+
+            root.AppendChild(createElement(xmldoc, "from", "taghreed"));
+
+
+            xmldoc.Save("C:/Users/Taghr/Documents/Test.xml");
+            Console.WriteLine(xmldoc.InnerXml);
+        }
+        public static XmlElement createElement(XmlDocument xmldoc, string element, string content)
+        {
+            XmlElement fromTag = xmldoc.CreateElement(element);
+            XmlText text1 = xmldoc.CreateTextNode(content);
+            fromTag.AppendChild(text1);
+            return fromTag;
+        }
     }
 }
